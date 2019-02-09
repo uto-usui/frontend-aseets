c61b33452c9b7c3b7c9f2a6c878e488686139fe0
uto-usui <me@uto-usui.com>
2019-02-10 01:49:29 +0900

    [package] Change stylelint package

--- eslint-babel-prettier/.stylelintrc.js
+++ eslint-babel-prettier/.stylelintrc.js
@@ -1,15 +1,8 @@
 module.exports = {
-  plugins: ['stylelint-scss'],
   extends: [
-    'stylelint-config-recess-order',
-    'stylelint-config-standard',
+    'stylelint-config-good-scss',
     './node_modules/prettier-stylelint/config.js'
   ],
-  ignoreFiles: [
-    '**/node_modules/**'
-  ],
   rules: {
-    'at-rule-no-unknown': null,
-    'scss/at-rule-no-unknown': true
   }
 }
