130e13b0e38d80205668ac3172ed23c4435eda66
uto-usui <me@uto-usui.com>
2019-11-08 16:57:17 +0900

    ⬆️ Update some packages

--- compress-image/index.js
+++ compress-image/index.js
@@ -1,17 +1,21 @@
-const imagemin = require('imagemin');
+const imagemin = require('imagemin-keep-folder');
 const imageminMozjpeg = require('imagemin-mozjpeg');
 const imageminPngquant = require('imagemin-pngquant');
 const imageminGifsicle = require('imagemin-gifsicle');
 const imageminSvgo = require('imagemin-svgo');
 
 (async () => {
-  await imagemin(['src/**/*.{jpg,png,gif,svg}'], 'dist', {
+  await imagemin(['src/**/*.{jpg,png,gif,svg}'], {
     plugins: [
       imageminMozjpeg({ quality: 80 }),
       imageminPngquant({ quality: [.65, .80], }),
       imageminGifsicle(),
       imageminSvgo()
-    ]
+    ],
+    replaceOutputDir: output => {
+      return output.replace(/src\//, 'dist/')
+    }
   });
-  console.log('Images optimized');
+  console.log('🍱 Images optimized 🍱');
 })();
+
