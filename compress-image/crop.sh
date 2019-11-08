4c588b83d1cf47741e6325a53760249010051aa4
uto-usui <me@uto-usui.com>
2019-11-08 16:58:34 +0900

    ✨ Add crop task

---
+++ compress-image/crop.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+
+# resize original images
+cd ./original
+sips --resampleWidth 1200  *.png
+
+# format from png to jpg
+mogrify -format jpg *.png
+rm *.png
+
+# crop original images
+cd ../
+mkdir crop
+cd ./original
+cp *.jpg ../crop/
+cd ../crop/
+mogrify -crop 1200x675+0+0 *.jpg
+find . -name "*.jpg" | while read file; do mv $file ${file/.jpg/_crop.jpg}; done
+
+# Put images in src folder
+cp *.jpg ../original/
+cd ../original/
+cp *.jpg ../src/
