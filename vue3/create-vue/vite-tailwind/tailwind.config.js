ce23eb68699da727b14701efb72962a671da2a44
uto-usui <me@uto-usui.com>
2022-01-12 21:37:33 +0900

    Add vite-tailwind

---
+++ vue3/create-vue/vite-tailwind/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  // https://tailwindcss.com/docs/configuration#core-plugins
+  corePlugins: ["textColor"],
+  theme: {
+    extend: {
+      //
+    },
+  },
+  variants: {},
+  plugins: [],
+};
